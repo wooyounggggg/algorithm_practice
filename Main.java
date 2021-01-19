5b809b6e4769483774d248a96ae352078122ee5e
wooyoung <souljit2@gmail.com>
2021-01-19 10:39:34 +0900

    BOJ 2751: 정렬문제 - 시간초과

--- Main.java
+++ Main.java
@@ -1,29 +1,25 @@
-public class Main {
-    private static int N;
-    private static int K;
-    private static long result;
-    private static int[] numList;
-    public static void main(String[] args){
-        Main app = new Main(args);
-        for(int i=0; i<N+1; i++) numList[i] = i;
-        app.solution(N, K, numList, 0);
-        System.out.println(result % 1000000000L);
-    }
-    public Main(String[] args){
-        N = Integer.parseInt(args[0]);
-        K = Integer.parseInt(args[1]);
-        numList = new int[N+1];
-        result = 0;
-    }
+import java.io.IOException;
+import java.util.Scanner;
 
-    public void solution(int targetSum, int elementNum, int[] usedNums, int seq){
-        if(targetSum < 0) return;
-        if(elementNum > 1)
-            for(int i:usedNums) solution(targetSum-i, elementNum - 1, usedNums, seq + 1);
-        else if(elementNum == 1 && targetSum <= N && hasElement(targetSum, usedNums)) result++;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] numList = new int[1000001];
+        for(int i=0; i<N; i++) numList[i] = sc.nextInt();
+        for(int i=0; i<N; i++) {
+            int tmp = numList[i];
+            int minIdx = minIdx(numList, i, N-1);
+            numList[i] = numList[minIdx];
+            numList[minIdx] = tmp;
+            System.out.println(numList[i]);
+        }
     }
-    public boolean hasElement(int element, int[] elements){
-        for(int i:elements) if(i==element) return true;
-        return false;
+    public static int minIdx(int[] numList, int begin, int end){
+        int minIdx = 0;
+        int i;
+        for(i=begin; i<end; i++)
+            minIdx = numList[i] < numList[minIdx] ? i : minIdx;
+        return minIdx;
     }
 }
