23a7499696ae6320b1fdca012a471df2ab737711
wooyoung <souljit2@gmail.com>
2021-01-19 10:40:26 +0900

    BOJ 2751 : StringBuilder를 사용하여 소요 시간을 줄임

--- Main.java
+++ Main.java
@@ -1,29 +1,17 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
-    private static int N;
-    private static int[] P;
-    private static int result;
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        Main app = new Main(sc);
-        app.solution(N);
-        System.out.println(result);
-        /*
-            dp[n] = dp[n-i] + dp[i]
-        */
-    }
-    public Main(Scanner sc){
-        N = sc.nextInt();
-        P = new int[N+1];
-        for(int i=1;i<=N;i++) P[i]=Integer.parseInt(sc.next());
-    }
-
-    public void solution(int N){
-        for(int i=0; i<N; i++) {
-            result += P[i];
-            if(N>0) solution(N - i);
-        }
+        StringBuilder sb = new StringBuilder();
+        int N = sc.nextInt();
+        ArrayList<Integer> numList = new ArrayList<>();
+        for(int i=0; i<N; i++) numList.add(sc.nextInt());
+        Collections.sort(numList);
+        for(int num:numList) sb.append(num).append("\n");
+        System.out.println(sb);
     }
 }
