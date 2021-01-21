c7f246ff7906e20b89d4c95c388bcc89f1e70909
wooyoung <souljit2@gmail.com>
2021-01-21 11:43:42 +0900

    BOJ 10989 : 시간 초과로 실패

--- Main.java
+++ Main.java
@@ -6,9 +6,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         StringBuilder sb = new StringBuilder();
-        ArrayList<Integer> numList = new ArrayList<>();
-        for(int i=0; i<N; i++) numList.add(sc.nextInt());
-        Collections.sort(numList);
+        int[] numList = new int[N];
+        for(int i=0; i<N; i++) numList[i] = sc.nextInt();
+        Arrays.sort(numList);
         for(int num:numList) sb.append(num+"\n");
         System.out.println(sb);
     }
