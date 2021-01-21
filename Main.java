4ff807d98040c97962469cbab2c882272caeb544
wooyoung <souljit2@gmail.com>
2021-01-21 11:43:20 +0900

    BOJ 10989 : 메모리 초과로 실패

--- Main.java
+++ Main.java
@@ -1,25 +1,15 @@
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] numList = new int[1000001];
-        for(int i=0; i<N; i++) numList[i] = sc.nextInt();
-        for(int i=0; i<N; i++) {
-            int tmp = numList[i];
-            int minIdx = minIdx(numList, i, N-1);
-            numList[i] = numList[minIdx];
-            numList[minIdx] = tmp;
-            System.out.println(numList[i]);
-        }
-    }
-    public static int minIdx(int[] numList, int begin, int end){
-        int minIdx = 0;
-        int i;
-        for(i=begin; i<end; i++)
-            minIdx = numList[i] < numList[minIdx] ? i : minIdx;
-        return minIdx;
+        StringBuilder sb = new StringBuilder();
+        ArrayList<Integer> numList = new ArrayList<>();
+        for(int i=0; i<N; i++) numList.add(sc.nextInt());
+        Collections.sort(numList);
+        for(int num:numList) sb.append(num+"\n");
+        System.out.println(sb);
     }
 }
