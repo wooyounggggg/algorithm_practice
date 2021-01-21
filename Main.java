901919716ff39204c426fc95eacacd390db57fda
wooyoung <souljit2@gmail.com>
2021-01-21 10:54:05 +0900

    BOJ 10825 : compareTo 사용하여 success

--- Main.java
+++ Main.java
@@ -5,19 +5,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[][] numList = new int[N][2];
-        for(int i=0; i<N; i++){
-            numList[i][0] = sc.nextInt();
-            numList[i][1] = sc.nextInt();
-        }
-        Arrays.sort(numList, new Comparator<int[]>() {
+        String[][] nameList = new String[N][4]; /* 이름 국어 영어 수학 */
+        for(int i=0; i<N; i++)
+            for(int j=0; j<4; j++)
+                nameList[i][j] = sc.next();
+        Arrays.sort(nameList, new Comparator<String[]>() {
             @Override
-            public int compare(int[] o1, int[] o2) {
-                if(o1[1]==o2[1]) return o1[0]-o2[0];
-                return o1[1]-o2[1];
+            public int compare(String[] o1, String[] o2) {
+                if(Integer.valueOf(o1[1]) == Integer.valueOf(o2[1])){
+                    if(Integer.valueOf(o1[2]) == Integer.valueOf(o2[2])) {
+                        if (Integer.valueOf(o1[3]) == Integer.valueOf(o2[3]))
+                            return o1[0].compareTo(o2[0]);
+                        return Integer.valueOf(o2[3]) - Integer.valueOf(o1[3]);
+                    }
+                    return Integer.valueOf(o1[2]) - Integer.valueOf(o2[2]);
+                }
+                return Integer.valueOf(o2[1]) - Integer.valueOf(o1[1]);
             }
         });
-        for(int[] num:numList)
-            System.out.println(num[0] + " " + num[1]);
+        for(String[] name:nameList) System.out.println(name[0]);
     }
 }
