24b57cbdd4ad93c21a572171646ea5b9b45eac1c
wooyoung <souljit2@gmail.com>
2021-02-02 04:49:48 +0900

    BOJ 11656 : 성공

--- Main.java
+++ Main.java
@@ -4,14 +4,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        String[] str = new String[4];
-        for(int i=0; i<2; i++){
-            str[i*2] = input.split(" ")[i*2];
-            str[i*2+1] = input.split(" ")[i*2+1];
-        }
-        long val1 = Long.parseLong(str[0]+str[1]);
-        long val2 = Long.parseLong((str[2]+str[3]));
-        System.out.println(val1+val2);
+        String str = sc.nextLine();
+        int length = str.length();
+        String[] subStrs = new String[length];
+        for(int i=0; i<length; i++) subStrs[i] = str.substring(i);
+        Arrays.sort(subStrs);
+        for(int i=0; i<length; i++)
+            System.out.println(subStrs[i]);
     }
 }
